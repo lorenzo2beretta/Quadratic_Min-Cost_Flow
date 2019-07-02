bcc18bacd88731d1d355a03fabc815ac92757898
Lorenzo <lorenzo.beretta@sns.it>
2019-07-02 17:25:53 +0200

    modularity enhanced

--- cg.py
+++ cg.py
@@ -9,7 +9,7 @@ from scipy.sparse.linalg import LinearOperator
 import time
 
 
-def conjugate_gradient(A, b, tol=1e-5, callback=None):
+def conjugate_gradient(A, b, tol=1e-5, maxiter=None, callback=None):
     ''' This function implements the conjugate gradient algorithm 
     solving A * x = b for a symmetric matrix A.
     '''
@@ -17,8 +17,14 @@ def conjugate_gradient(A, b, tol=1e-5, callback=None):
     r = np.array(b)                      # current residual value
     p = np.array(b)                      # current update direction
     rr = np.dot(r, r)                    # error squared norm
+    itn = 0
+    info = 0
     
     while np.sqrt(rr) > tol:
+
+        if maxiter and itn > maxiter:    # method not converged
+            info = maxiter
+            break
         
         Ap = A * p                       # fast matrix-vector product
         alpha = rr / np.dot(p, Ap)       # step length
@@ -31,25 +37,18 @@ def conjugate_gradient(A, b, tol=1e-5, callback=None):
         beta = rr / rr_old               # step improvement
         p = r + beta * p                 # next update direction
 
+        itn += 1
         callback(x)
 
-    return x
-
-
-def solve(edges, b, tol=1e-5, algo=conjugate_gradient):
-    ''' This function solves uncapacited and undirected quadratic 
-    separable MCF, equivalent to the solution of the linear system 
     
-    E * D^-1 * E^t * x = b
+    return x, info
 
-    where E is the edge-node matrix and D is the diagonal matrix
-    containing the edges' weights.
 
-    It is parametric in the algorithm used to solve that linear
-    system in order to compare different solutions.
-    '''    
+def make_operator(edges, n):
+    ''' This function returns a LinearOperator object performing
+    the product x -> E^t * D^-1 * E * x
+    '''
     edges = [(e[0], e[1], 1 / float(e[2])) for e in edges]
-    b = [float(z) for z in b]
     
     def lin_op(x):
         ''' Fast matrix-vector multiplication exploiting the structure
@@ -63,22 +62,75 @@ def solve(edges, b, tol=1e-5, algo=conjugate_gradient):
 
         return res
 
+    A = LinearOperator((n, n), matvec=lin_op)
+    return A 
+
+
+def get_primal(edges, dual):
+    ''' This function reconstruct primal solution given the dual one.
+    '''
+    primal = [(dual[e[1]] - dual[e[0]]) / e[2] for e in edges]
+    return np.array(primal)
+
+
+def solve(edges, b, tol=1e-5, algo=conjugate_gradient):
+    ''' This function solves uncapacited and undirected quadratic 
+    separable MCF, equivalent to the solution of the linear system 
+    
+    A * x = b,    with    A = E^t * D^-1 * E
+
+    where E is the edge-node matrix and D is the diagonal matrix
+    containing the edges' weights.
+
+    It is parametric in the algorithm used to solve that linear
+    system in order to compare different solutions.
+    '''    
+    A = make_operator(edges, len(b))  # defining linear operator
+
     itn = 0
-    def callback(xk):  # callback function to count iterations
+    def callback(xk):  # counting iterations
         nonlocal itn
         itn += 1
 
-    n = len(b)
-    A = LinearOperator((n, n), matvec=lin_op)   # define x -> A * x
-
     t0 = time.time()
-    x = algo(A, b, tol=tol, callback=callback)  # solve A * x = b
+    x, info = algo(A, b, tol=tol, callback=callback)  # solve A * x = b
     t1 = time.time()
     tspan = t1 - t0  # measuring elapsed time
-    
-    if algo == sp.sparse.linalg.cg:  # off-the-shelf CG nasty signature
-        x = x[0]
-        
-    f = [e[2] * (x[e[1]] - x[e[0]]) for e in edges]  # primal solution
 
-    return x, np.array(f), itn, tspan
+    f = get_primal(edges, x)
+
+    return x, f, itn, tspan, info
+
+
+def read_DIMACS(file_path):
+    ''' This method reads the topology and costs of an undirected 
+    graph form a file following the DIMACS Min-Cost flow convention.
+    '''
+    file = open(file_path, 'r')
+    edges = []
+    
+    while True:
+        line = file.readline()
+        if line == '':
+            break
+
+        if line[:1] == 'c':
+            continue
+
+        if line[:1] == 'p':
+            token = line.split()
+            n = int(token[2]) + 1
+            b = [0] * n
+            
+        if line[:1] == 'n':
+            token = line.split()
+            b[int(token[1])] = float(token[2])
+
+        if line[:1] == 'a':
+            token = line.split()
+            u = int(token[1])
+            v = int(token[2])
+            c = float(token[5])
+            edges.append((u, v, c))
+
+    return edges, b
--- test.py
+++ test.py
@@ -1,40 +1,7 @@
 from cg import solve
+from cg import read_DIMACS
 import scipy as sp
 import numpy as np
-
-def read_DIMACS(file_path):
-    ''' This method reads the topology and costs of an undirected 
-    graph form a file following the DIMACS Min-Cost flow convention.
-    '''
-    file = open(file_path, 'r')
-    edges = []
-    
-    while True:
-        line = file.readline()
-        if line == '':
-            break
-
-        if line[:1] == 'c':
-            continue
-
-        if line[:1] == 'p':
-            token = line.split()
-            n = int(token[2]) + 1
-            b = [0] * n
-            
-        if line[:1] == 'n':
-            token = line.split()
-            b[int(token[1])] = float(token[2])
-
-        if line[:1] == 'a':
-            token = line.split()
-            u = int(token[1])
-            v = int(token[2])
-            c = float(token[5])
-            edges.append((u, v, c))
-
-    return edges, b
-
 
 file_path_1000 = 'mcf_generator/1000/netgen-1000-3-5-b-b-s.dmx'
 file_path_2000 = 'mcf_generator/2000/netgen-2000-3-5-b-b-s.dmx'
@@ -49,9 +16,9 @@ r = 100000
 
 edges = [(e[0], e[1], np.random.uniform(l, r)) for e in edges]
 
-x1, f1, itn1, t1 = solve(edges, b, 1e-10)
+x1, f1, itn1, t1, info1 = solve(edges, b, 1e-10)
 
-x2, f2, itn2, t2 = solve(edges, b, 1e-10, sp.sparse.linalg.cg)
+x2, f2, itn2, t2, info2 = solve(edges, b, 1e-10, sp.sparse.linalg.cg)
 
 x_err_abs = np.linalg.norm(x1 - x2)
 x_err_rel = x_err_abs / np.linalg.norm(x2)
@@ -70,6 +37,3 @@ print("x_err_rel = " + x_err_rel.__str__())
 
 print("f_err_abs = " +  f_err_abs.__str__())
 print("f_err_rel = " + f_err_rel.__str__())
-
-
-            
